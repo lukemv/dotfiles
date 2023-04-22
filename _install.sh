9017d0a3a14d3ebcc5c2c34339e0580060a21ddf
lukem <luke@lukemuccillo.com>
2023-04-22 15:08:21 +1000

    feat: add gh cli configuration
    Closes #3

--- _install.sh
+++ _install.sh
@@ -6,10 +6,28 @@ function join() {
   echo "$*"
 }
 
-function make_symlnks() {
+function install_configs() {
+  echo "[!] Installing config files"
+  local thisdir=$(pwd)
+  local configs=("gh/config.yml")
+  for filepath in "${configs[@]}"
+  do
+    echo " [-] installing $filepath"
+    symlink="${HOME}/.config/${filepath}"
+    realfile="${thisdir}/config/${filepath}"
+    [ -e $symlink  ] && rm -rf $symlink
+    ln -s $realfile $symlink
+  done
+  echo "[!] All configs are installed"
+  echo "-------------------------------"
+  # ls -allh ~/ | grep -E "($(join '|' ${dotfiles[@]}))"
+  echo "-------------------------------"
+}
+
+function install_dotfiles() {
   echo "[!] Installing dotfiles"
   local thisdir=$(pwd)
-  dotfiles=("bashrc" "zshrc" "zshrc.d" "tmux.conf" "vimrc" "gitconfig" "gitconfig.d")
+  local dotfiles=("bashrc" "zshrc" "zshrc.d" "tmux.conf" "vimrc" "gitconfig" "gitconfig.d")
   for file in "${dotfiles[@]}"
   do
     echo " [-] installing $file"
@@ -24,4 +42,5 @@ function make_symlnks() {
   echo "-------------------------------"
 }
 
-make_symlnks
+install_dotfiles
+install_configs
