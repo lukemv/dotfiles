c40099a25eaf343782a28906ddc0acf4ab2c45f2
lukem <luke@lukemuccillo.com>
2024-05-08 06:45:42 +1000

    chore: remove unused aliases

--- zshrc.d/alias.zsh
+++ zshrc.d/alias.zsh
@@ -6,7 +6,6 @@ alias kns="kubectl ns"
 alias tf="terraform"
 alias commit="git rev-parse --short HEAD"
 alias fd='fdfind'
-alias gri="git rebase --interactive"
 alias gp="git push"
 alias gpf="git push --force"
 alias gs="git stash"
@@ -14,4 +13,3 @@ alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %
 alias gres="git checkout main && git pull --rebase"
 alias mres="git checkout master && git pull --rebase"
 alias gsa="git stash apply"
-alias dev="ssh sa-luke.muccillo@syd-it-vl016"
