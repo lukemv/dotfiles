1d33212d20a236f15b21cfe1dbe62acf66da291e
lukem <x@mucc.au>
2024-07-02 03:44:46 +0000

    feat: remove unused git conflict plugin + document bufferline

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -6,7 +6,7 @@ map("i", "jk", "<esc>")
 -- Quick access to some common actions
 map("n", "<leader>fw", "<cmd>w<cr>", "Write")
 -- map("n", "<leader>fa", "<cmd>wa<cr>", "Write all")
-map("n", "<leader>qq", "<cmd>q<cr>", "Quit")
+map("n", "<leader>qq", "<cmd>q!<cr>", "Quit")
 -- map("n", "<leader>qa", "<cmd>qa!<cr>", "Quit all")
 -- map("n", "<leader>dw", "<cmd>close<cr>", "Window")
 
