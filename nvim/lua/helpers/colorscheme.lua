86d26c91a4e598e237bc809b577e4d360dbfec7b
lukem <x@mucc.au>
2024-07-05 20:51:52 +1000

    feat: rose-pine is the way forward :)

--- nvim/lua/helpers/colorscheme.lua
+++ nvim/lua/helpers/colorscheme.lua
@@ -16,10 +16,10 @@ local function get_if_available(name, opts)
 end
 
 -- Uncomment the colorscheme to use
-local colorscheme = get_if_available("tokyonight")
+-- local colorscheme = get_if_available("tokyonight")
 -- local colorscheme = get_if_available("catppuccin")
 -- local colorscheme = get_if_available('gruvbox')
--- local colorscheme = get_if_available('rose-pine')
+local colorscheme = get_if_available('rose-pine')
 -- local colorscheme = get_if_available('everforest')
 -- local colorscheme = get_if_available('melange')
 
