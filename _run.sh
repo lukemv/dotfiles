d7e35b96dcc7ce1c3339733a37f38c65bf14ab1b
lukem <x@mucc.au>
2024-07-10 23:52:03 +0000

    feat: add debian dockerfile
    Two linux flavours are supported, which covers most of the
    dev stations that I'll be developing on.

--- _run.sh
+++ _run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker build -t dotfiles .
+target=$1
+docker build -f "Dockerfile-$target" -t dotfiles .
 docker run -it -v "$(pwd):/home/me/dotfiles" dotfiles /usr/bin/zsh
 echo "[**] The container has exited"
