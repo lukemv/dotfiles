87303254fb61a047a111e06076fc8ad521f6130e
lukem <x@mucc.au>
2024-07-11 02:13:47 +0000

    feat: fix zsh history stuff

---
+++ zshrc.d/history.zsh
@@ -0,0 +1,14 @@
+HISTFILE=~/.zsh_history
+# history limit of the file on disk
+export HISTFILESIZE=10000000 
+# current session's history limit, 
+# also following this https://unix.stackexchange.com/a/595475 $HISTSIZE should be at least 20% bigger than $SAVEHIST 
+export HISTSIZE=20000000
+setopt INC_APPEND_HISTORY 
+setopt SHARE_HISTORY 
+# records the time when each command was executed along with the command 
+setopt EXTENDED_HISTORY 
+# ensures that each command entered in the current session is appended to the history file immediately after execution
+setopt APPENDHISTORY 
+export HISTTIMEFORMAT="%d/%m/%Y %H:%M] 
+
