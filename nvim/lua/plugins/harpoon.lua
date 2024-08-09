530af4a7c32ec4e849476d96d465c515396fbf1f
Ciano <ciano@Cianos-MacBook-Pro.local>
2024-08-09 21:49:30 +1000

    feat: add some harpoon bindings
    These probably won't work over ssh but I'm really feeling hamstrung
    not being able to quickly change between commonly used buffer.

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -18,12 +18,12 @@ return {
 		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 		-- Note: I can't seem to find good keys for these :D
-		-- vim.keymap.set("n", "<S-h>", function() harpoon:list():select(1) end)
-		-- vim.keymap.set("n", "<S-j>", function() harpoon:list():select(2) end)
-		-- vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-		-- vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<S-h>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<S-j>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<S-k>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<S-l>", function() harpoon:list():select(4) end)
 		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<S-h>", function() harpoon:list():prev() end)
-		vim.keymap.set("n", "<S-l>", function() harpoon:list():next() end)
+		-- vim.keymap.set("n", "<S-h>", function() harpoon:list():prev() end)
+		-- vim.keymap.set("n", "<S-l>", function() harpoon:list():next() end)
 	end
 }
