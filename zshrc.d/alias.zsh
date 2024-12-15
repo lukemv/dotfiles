f18f7a0cddf6d6789c0fdca909d14f112f3b746b
lukem <x@mucc.au>
2024-12-16 10:45:11 +1100

    feat: add single command for git reset

--- zshrc.d/alias.zsh
+++ zshrc.d/alias.zsh
@@ -12,12 +12,26 @@ alias gs="git stash"
 alias gsa="git stash apply"
 alias gfa="git fetch --all"
 alias gcur="git rev-parse --short HEAD"
-alias gc="git commit --amend"
 
 alias dps="docker ps"
 
-alias gres="git checkout main && git pull --rebase"
-alias mres="git checkout master && git pull --rebase"
+function res() {
+  default_branch=$(git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's|^refs/remotes/origin/||')
+
+  if [[ -z $default_branch ]]; then
+    if git show-ref --verify --quiet refs/heads/main; then
+      default_branch="main"
+    elif git show-ref --verify --quiet refs/heads/master; then
+      default_branch="master"
+    else
+      echo "Could not determine default branch."
+      return 1
+    fi
+  fi
+
+  # Now checkout and pull with rebase
+  git checkout "$default_branch" && git pull --rebase
+}
 # Kubernetes aliases
 alias k="kubectl"
 alias kctx="kubectl ctx"
