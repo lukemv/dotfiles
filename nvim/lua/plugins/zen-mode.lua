ff72667ddb00f6b4fbdf0475bbc623614ab77757
lukem <x@mucc.au>
2025-01-21 14:04:37 +1100

    feat: zen mode takes up full screen

--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -3,8 +3,8 @@ return {
 	config = function()
 		require("zen-mode").setup {
 			window = {
-				width = 0.85, -- width will always be 85% of the editor width
-				height = 0.85, -- height will always be 85% of the editor height
+				width = 1,
+				height = 1,
 			},
 		}
 	end,
