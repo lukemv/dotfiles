22267cf32d55bd5ff7c6c564c44235232adb147b
lukem <x@mucc.au>
2025-01-24 08:29:04 +1100

    feat: make ssh-agent setup not annoying

--- zshrc.d/ssh-agent.zsh
+++ zshrc.d/ssh-agent.zsh
@@ -1,3 +1,25 @@
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+#!/bin/bash
+AGENT_ENV="$HOME/.ssh/agent.env"
+
+# Start agent if not already running
+start_agent() {
+   (umask 077; ssh-agent > "$AGENT_ENV")
+   . "$AGENT_ENV" > /dev/null
+}
+
+# Check if the agent is already running
+if [ -f "$AGENT_ENV" ]; then
+   . "$AGENT_ENV" > /dev/null
+   # Check if agent is dead and start it again if needed
+   if ! ssh-add -l > /dev/null 2>&1; then
+       start_agent
+   fi
+else
+   start_agent
+fi
+
+# Add keys if not already added
+if ! ssh-add -l | grep -q "$(ssh-keygen -lf ~/.ssh/id_ed25519.pub | awk '{print $2}')"; then
+   ssh-add ~/.ssh/id_ed25519
+fi
 
