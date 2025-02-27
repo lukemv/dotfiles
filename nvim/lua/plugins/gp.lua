e82bc7e6bd56b94d34d7909f69dea62e7560a47a
lukem <x@mucc.au>
2025-02-27 11:03:19 +1100

    feat: add gp.nvim configuration

--- nvim/lua/plugins/gp.lua
+++ nvim/lua/plugins/gp.lua
@@ -2,10 +2,14 @@ return {
 	"robitx/gp.nvim",
 	config = function()
 		local conf = {
-			-- For customization, refer to Install > Configuration in the Documentation/Readme
+			providers = {
+				openai = {
+					endpoint = "https://api.openai.com/v1/chat/completions",
+					secret = os.getenv("OPENAI_API_KEY"),
+				},
+			}
 		}
 		require("gp").setup(conf)
-
 		-- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
 	end,
 }
