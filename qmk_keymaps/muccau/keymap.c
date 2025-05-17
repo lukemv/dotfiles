22ad48ed649f927051aadff9ee02fe70b67e6ad0
lukemv <x@mucc.au>
2025-05-18 09:30:08 +1000

    feat: use default voyager keymap on row 1
    I think I just got really used to the small advantages in my key map
    but I was missing a lot of keys. I'll try this keymap to see if the
    symbol positions are actally more conducive to productivity

--- qmk_keymaps/muccau/keymap.c
+++ qmk_keymaps/muccau/keymap.c
@@ -15,8 +15,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_1,    KC_8,    KC_9,    KC_MINS, KC_SLSH, KC_F12,
-        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_LBRC,                      KC_RBRC,   KC_MINUS,    KC_6,    KC_PLUS, KC_ASTR, KC_BSPC,
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_7,    KC_8,    KC_9,    KC_MINS, KC_SLSH, KC_F12,
+        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                      KC_4,    KC_5,    KC_6,    KC_PLUS, KC_ASTR, KC_BSPC,
         _______, _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,                      KC_1,    KC_2,    KC_3,    KC_DOT,  KC_EQL,  KC_ENT,
                                                      _______, _______,    _______, KC_0
     ),
