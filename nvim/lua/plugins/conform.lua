d06e350f37f0f3e39ce72ae6f3114fa2c8b23706
lukemv <x@mucc.au>
2025-05-20 13:16:14 +1000

    feat: format conform.lua :)

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,18 +1,18 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
-	setup = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				terraform = { "terraform_fmt" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				rust = { "rustfmt", lsp_format = "fallback" },
-				-- Conform will run the first available formatter
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-			},
-		})
-	end,
+    'stevearc/conform.nvim',
+    opts = {},
+    setup = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                terraform = { "terraform_fmt" },
+                -- Conform will run multiple formatters sequentially
+                python = { "isort", "black" },
+                -- You can customize some of the format options for the filetype (:help conform.format)
+                rust = { "rustfmt", lsp_format = "fallback" },
+                -- Conform will run the first available formatter
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+            },
+        })
+    end,
 }
