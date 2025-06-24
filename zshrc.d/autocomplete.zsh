1a584d647efce2a6efa896f86d4b4f25f419869b
lukemv <x@mucc.au>
2025-06-25 08:50:49 +1000

    feat: misc dotfiles  updates

--- zshrc.d/autocomplete.zsh
+++ zshrc.d/autocomplete.zsh
@@ -1,4 +1,2 @@
-[[ -r ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]] ||
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
+
--- zshrc.d/pyenv.zsh
+++ zshrc.d/pyenv.zsh
@@ -1,7 +1,6 @@
-if command -v pyenv >/dev/null; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-  export PYENV_ROOT="$HOME/.pyenv"
-  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-fi
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
 
