3213ad846560aabe69f6f2a37c0270ae4c92e15d
lukemv <x@mucc.au>
2025-07-07 19:59:35 +1000

    feat: a lot of dotfiles updated

--- qmk_keymaps/muccau/keymap.c
+++ qmk_keymaps/muccau/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_EQUAL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_LSFT, KC_A, KC_S, KC_D, MT(MOD_LCTL, KC_F), KC_G,
                                    KC_H, MT(MOD_RCTL, KC_J), KC_K, MT(MOD_RGUI, KC_L), KC_SCLN, RSFT_T(KC_QUOT),
         KC_LCTL, MT(MOD_LALT, KC_Z), KC_X, KC_C, KC_V, KC_B,                             KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH, KC_ESC,
                                 KC_ENT, LT(1, KC_TAB),              KC_BSPC, LT(2, KC_SPC)
@@ -28,7 +28,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      _______, _______,    _______, _______
     ),
 };
-
-
 
 
