c22caff98405d2e5cdbadecfedd4ef6632ed9475
Luke Muccillo <luke.muccillo@lifetrading.com.au>
2025-12-17 12:00:51 +1100

    feat: remove fzf shell integration
    I prefer atuin, it's way better

--- zshrc.d/fzf.zsh
+++ zshrc.d/fzf.zsh
@@ -9,5 +9,3 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-# Load fzf shell integration
-[ -f /usr/share/fzf/shell/key-bindings.zsh ] && source /usr/share/fzf/shell/key-bindings.zsh
