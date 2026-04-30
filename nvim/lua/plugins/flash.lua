68a4193cfee9f454279974f0878a8fe3caad666c
lukem <x@mucc.au>
2026-04-30 21:51:00 +1000

    chore: add flash.nvim

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,45 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {
+		modes = {
+			-- Enhance `/` and `?` with labeled jump targets
+			search = { enabled = true },
+			-- Show labels while typing `f`/`F`/`t`/`T`
+			char = { enabled = true },
+		},
+	},
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function() require("flash").jump() end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function() require("flash").treesitter() end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"r",
+			mode = "o",
+			function() require("flash").remote() end,
+			desc = "Remote Flash",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function() require("flash").treesitter_search() end,
+			desc = "Treesitter Search",
+		},
+		{
+			"<c-s>",
+			mode = { "c" },
+			function() require("flash").toggle() end,
+			desc = "Toggle Flash Search",
+		},
+	},
+}
