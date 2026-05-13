cb17c47117107fa1e8b1bb38c0e6d95ce21ded8c
Luke Muccillo <luke.muccillo@lifetrading.com.au>
2026-05-13 22:02:18 +1000

    feat: add marks and update tmux theme
    - I'm evaluating this marks plugin
    - I got tired of the same catpuccin theme

---
+++ nvim/lua/plugins/marks.lua
@@ -0,0 +1,15 @@
+return {
+    "chentoast/marks.nvim",
+    event = "BufRead",
+    config = function()
+        require("marks").setup({
+            default_mappings = true,
+            builtin_marks = { ".", "<", ">", "^" },
+            cyclic = true,
+            force_write_shada = false,
+            refresh_interval = 250,
+            sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+            bookmark_0 = { sign = "⚑", virt_text = "", annotate = false },
+        })
+    end
+}
